8f5e2eb2e678b3a7be2a53a93375d4fb88d135a4
umutesimagnificat@gmail.com <umutesimagnificat@gmail.com>
2024-02-15 14:25:27 +0200

    read a file

--- filesystem.js
+++ filesystem.js
@@ -1,5 +1,9 @@
+const fs = require('fs')
 //read file
-
+fs.readFile('./people.js',(error,data)=>{
+    if(error)throw error
+    console.log(data.toString())
+})
 //write file
 
 //create directory
