ba52ca8149be33434d1ab5d0fa2d0fa68bbe1a52
umutesimagnificat@gmail.com <umutesimagnificat@gmail.com>
2024-02-16 19:24:08 +0200

    add status codes

--- clients-servers/server.js
+++ clients-servers/server.js
@@ -1,31 +1,35 @@
 const http = require("http");
-const fs = require('fs');
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log("request was made");
   //request object
-  
-//   console.log(req.url, req.method);
-let path = './clients-servers/content/'
-switch (req.url){
-    case '/':
-    path+='index.html'
-    break;
-    case '/about':
-    path+='about.html'
-    break;
-    default :
-    path+='notfound.html'
-}
+
+  //   console.log(req.url, req.method);
+  let path = "./clients-servers/content/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "notfound.html";
+      res.statusCode = 400;
+  }
   //response object
   console.log(res);
   res.setHeader("content-Type", "text/html");
-  fs.readFile(path,(err,data)=>{
-    if(err)throw err
-    else{
-        res.write(data)
-        res.end()
+  fs.readFile(path, (err, data) => {
+    if (err) throw err;
+    else {
+      // res.write(data)
+      // res.end()
+      res.end(data);
     }
-  })
+  });
 });
 
 server.listen(3000, "localhost", () => {
